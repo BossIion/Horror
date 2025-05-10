2278af438cd8d67a57feebfdc0d29efaf8dd3c92
BossIion <ulysse.schaff@gmail.com>
2025-05-10 12:05:15 -0400

    cool

--- script/opendoor.gd
+++ script/opendoor.gd
@@ -1,5 +1,15 @@
-extends Node3D
-func _on_body_entered(body):
+extends Area3D
+var Key_Touched = false
+func _on_body_entered(body):	
 	print("Body entered: ", body.name)  # Debugging: print the body name
-	if body.name == "Key":  # Check if the body is the player
-		queue_free()
+	if body.name == "Player":  # Check if the body is the player
+		for i in range(0,91):
+			$PivotPoin.rotation = Vector3(0, -deg_to_rad(i),0)
+			$CollisionShape3D.rotation = $PivotPoint/CSGBox3D.rotation
+			$CollisionShape3D.global.position = $PivotPoint/CSGBox3D.global.position
+			await get_tree().create_timer(0.1).timeout
+		$PivotPoin.rotation = Vector3(0, -deg_to_rad(-90),0)
+		$CollisionShape3D.rotation = $PivotPoint/CSGBox3D.rotation
+		$CollisionShape3D.global.position = $PivotPoint/CSGBox3D.global.position
+
+		
