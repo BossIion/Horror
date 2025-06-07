cd902f988e5444399d33fc8ee5efab0ee484f709
BossIion <ulysse.schaff@gmail.com>
2025-06-07 12:29:46 -0400

    AI

--- scenes/scary_person.gd
+++ scenes/scary_person.gd
@@ -1,10 +1,32 @@
 extends Node3D
+
+var enemySpeed = 100
+
 func die(body):
 	if body.name == "Player":
 		var root = get_tree().get_root()
 		var screen = $"../Death_Screen"
-
 		screen.visible = true
-
+		await get_tree().create_timer(2).timeout
+		screen.visible = false
+		get_tree().reload_current_scene()
+		if Input.is_anything_pressed() == true:
+			pass
 func _on_scary_body_entered(body: Node3D) -> void:
 	die(body)
+func follow_player(delta):
+	var player: CharacterBody3D = $"../Player"
+	$Scary3.look_at(player.global_position)
+	$Scary3.rotation[0] = 0
+	var dirVector = (player.global_position - $Scary3.global_position).normalized()
+	print(dirVector)
+	dirVector[1] = 0
+	
+	$Scary3.global_position = $Scary3.global_position + (dirVector * enemySpeed/10000)
+	
+	
+func _ready() -> void:
+	pass 
+
+func _process(delta: float) -> void:
+	follow_player(delta)
